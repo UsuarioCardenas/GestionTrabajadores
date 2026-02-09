217dfe899f57e212cf32b0a437c50efe8b9e113e
UsuarioCardenas <diego.cardenas.g@tecsup.edu.pe>
2026-02-09 04:24:01 -0500

    feat(infrastructure): configure Entity Framework and DbContext

---
+++ src/Backend/GestiónTrabajadores.Infrastructure/Data/Configurations/TrabajadorConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using GestiónTrabajadores.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GestiónTrabajadores.Infrastructure.Data.Configurations;
+
+public class TrabajadorConfiguration : IEntityTypeConfiguration<Trabajador>
+{
+    public void Configure(EntityTypeBuilder<Trabajador> builder)
+    {
+        builder.ToTable("Trabajadores");
+
+        builder.HasKey(t => t.IdTrabajador);
+
+        builder.Property(t => t.IdTrabajador)
+            .HasColumnName("IdTrabajador")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(t => t.Nombres)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(t => t.Apellidos)
+            .IsRequired()
+            .HasMaxLength(50);
+
+        builder.Property(t => t.TipoDocumento)
+            .IsRequired()
+            .HasMaxLength(35);
+
+        builder.Property(t => t.NumeroDocumento)
+            .IsRequired()
+            .HasMaxLength(35);
+
+        builder.HasIndex(t => t.NumeroDocumento)
+            .IsUnique()
+            .HasDatabaseName("IX_Trabajadores_NumeroDocumento");
+
+        builder.Property(t => t.Sexo)
+            .IsRequired()
+            .HasMaxLength(1)
+            .HasColumnType("CHAR(1)");
+
+        builder.Property(t => t.FechaNacimiento)
+            .IsRequired()
+            .HasColumnType("DATE");
+
+        builder.Property(t => t.Foto)
+            .HasMaxLength(500);
+
+        builder.Property(t => t.Direccion)
+            .IsRequired()
+            .HasMaxLength(175);
+
+        builder.Ignore(t => t.NombreCompleto);
+    }
+}
---
+++ src/Backend/GestiónTrabajadores.Infrastructure/Data/TrabajadoresDbContext.cs
@@ -0,0 +1,19 @@
+﻿using GestiónTrabajadores.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestiónTrabajadores.Infrastructure.Data;
+
+public class TrabajadoresDbContext : DbContext
+{
+    public TrabajadoresDbContext(DbContextOptions<TrabajadoresDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Trabajador> Trabajadores { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(TrabajadoresDbContext).Assembly);
+    }
+}
